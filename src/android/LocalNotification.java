1eea2780e004127891bf17265332fc4dc408c487
an-rohangovekar <rohan.g@applicationnexus.com>
2016-01-08 14:46:44 +0530

    Update LocalNotification.java

--- src/android/LocalNotification.java
+++ src/android/LocalNotification.java
@@ -243,8 +243,9 @@ public class LocalNotification extends CordovaPlugin {
         for (int i = 0; i < notifications.length(); i++) {
             JSONObject options;
             try {
-                Log.v("ConsoleLog", "notification in schedule :"+notifications[i]);
+                
                 options = notifications.optJSONObject(i);
+                Log.v("ConsoleLog", "notification in schedule :"+options);
                 
             } catch (JSONException e){
                 Log.v("ConsoleLog", "error in schedule :"+e);
